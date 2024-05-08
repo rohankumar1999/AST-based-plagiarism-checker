3b4b368259f54c82d49a4ad81d05b84f4e87c376
Rohan Kumar <nagarohankumar.bayya@sjsu.edu>
2024-05-07 20:27:43 -0700

    project structure

---
+++ app/Main.hs
@@ -0,0 +1,18 @@
+module Main where
+
+import Control.Monad (void)
+import Ast
+import Compare
+
+
+
+
+main :: IO ()
+main = do
+    list1 <- showAst "abs.imp"
+    list2 <- showAst "abs1.imp"
+    -- print list1
+    print list2
+    let overlaps = findOverlappingSubarrays list1 list2
+    print "Number of overlaps: "
+    print $ overlaps
